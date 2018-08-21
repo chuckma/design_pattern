0548a60fe579e9c24dbbdce41bdd1f9df4a3d574
chuckma <robbincen@163.com>
2018-08-21 11:00:10 +0800

    resolve serializable destroy singleton

--- src/main/java/cn/lucasma/design/pattern/creational/singleton/HungrySingleton.java
+++ src/main/java/cn/lucasma/design/pattern/creational/singleton/HungrySingleton.java
@@ -1,5 +1,7 @@
 package cn.lucasma.design.pattern.creational.singleton;
 
+import java.io.Serializable;
+
 /**
  * @author lucas
  * <p>
@@ -8,9 +10,11 @@ package cn.lucasma.design.pattern.creational.singleton;
  * 没有延迟加载的效果
  * 如果这个类没有使用，就造成了内存的浪费。
  */
-public class HungrySingleton {
+public class HungrySingleton implements Serializable{
 
-    //类加载的时候就完成实例化 final 使得类不可更改
+    /**
+     * 类加载的时候就完成实例化 final 使得类不可更改
+     */
     private final static HungrySingleton hungrySingleton;
 
     static {
@@ -23,6 +27,14 @@ public class HungrySingleton {
     }
 
     public static HungrySingleton getInstance() {
+        return hungrySingleton;
+    }
+
+    /**
+     * 序列化和反序列化 之后的对象不是同一个对象，下面这个方法可以解决这个问题
+     * @return
+     */
+    private Object readResolve() {
         return hungrySingleton;
     }
 }
--- src/main/java/cn/lucasma/design/pattern/creational/singleton/Test.java
+++ src/main/java/cn/lucasma/design/pattern/creational/singleton/Test.java
@@ -1,17 +1,30 @@
 package cn.lucasma.design.pattern.creational.singleton;
 
+import java.io.*;
+
 /**
  * Author: lucasma
  */
 public class Test {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
 //        LazySingleton lazySingleton = LazySingleton.getInstance();
 
-        Thread t1 = new Thread(new T());
-        Thread t2 = new Thread(new T());
-        t1.start();
-        t2.start();
-        System.out.println("program end");
+//        Thread t1 = new Thread(new T());
+//        Thread t2 = new Thread(new T());
+//        t1.start();
+//        t2.start();
+//        System.out.println("program end");
+        HungrySingleton instance = HungrySingleton.getInstance();
+        // 如果将这个 instance 序列号到一个文件中，然后再从这个文件中取出来，这两个对象还是同一个对象吗？
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("single_file"));
+        oos.writeObject(instance);
+
+        File file = new File("single_file");
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file));
+        HungrySingleton newInstance = (HungrySingleton) ois.readObject();
+        System.out.println(instance);
+        System.out.println(newInstance);
+        System.out.println(instance == newInstance);
     }
 }
