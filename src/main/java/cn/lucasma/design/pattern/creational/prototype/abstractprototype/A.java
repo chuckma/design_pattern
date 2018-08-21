d5fa7c56d7f2abe09ce1cc4301f0b61242c596bc
lucas ma <robbincen@163.com>
2018-08-21 22:50:54 +0800

    prototype

---
+++ src/main/java/cn/lucasma/design/pattern/creational/prototype/abstractprototype/A.java
@@ -0,0 +1,11 @@
+package cn.lucasma.design.pattern.creational.prototype.abstractprototype;
+
+/**
+ * Created by geely
+ */
+public abstract class A implements Cloneable{
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
