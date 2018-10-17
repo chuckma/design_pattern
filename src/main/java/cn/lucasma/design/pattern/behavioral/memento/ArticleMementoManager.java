52670ddca14edaf617cc58cd338a7d2ad606dbff
lucas ma <robbincen@163.com>
2018-10-17 21:09:19 +0800

    momento

---
+++ src/main/java/cn/lucasma/design/pattern/behavioral/memento/ArticleMementoManager.java
@@ -0,0 +1,21 @@
+package cn.lucasma.design.pattern.behavioral.memento;
+
+import java.util.Stack;
+
+/**
+ * Created by lucasma
+ */
+public class ArticleMementoManager {
+
+
+    private final Stack<ArticleMemento> ARTICLE_MEMENTO_STACK = new Stack<ArticleMemento>();
+
+    public ArticleMemento getMemento() {
+        ArticleMemento articleMemento = ARTICLE_MEMENTO_STACK.pop();
+        return articleMemento;
+    }
+
+    public void addMemento(ArticleMemento articleMemento) {
+        ARTICLE_MEMENTO_STACK.push(articleMemento);
+    }
+}
