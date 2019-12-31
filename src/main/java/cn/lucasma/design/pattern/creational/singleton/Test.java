bd8bac92cf6f3f9537765f66baca648e55ae2438
lucas ma <robbincen@163.com>
2019-12-31 21:18:38 +0800

    add readme

--- src/main/java/cn/lucasma/design/pattern/creational/singleton/Test.java
+++ src/main/java/cn/lucasma/design/pattern/creational/singleton/Test.java
@@ -11,7 +11,7 @@ public class Test {
 
     public static void main(String[] args) throws IOException, ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
 //        LazySingleton lazySingleton = LazySingleton.getInstance();
-        System.out.println(ThreadLocalInstance.getInstance());
+       /* System.out.println(ThreadLocalInstance.getInstance());
         System.out.println(ThreadLocalInstance.getInstance());
         System.out.println(ThreadLocalInstance.getInstance());
         System.out.println(ThreadLocalInstance.getInstance());
@@ -19,7 +19,7 @@ public class Test {
         Thread t2 = new Thread(new T());
         t1.start();
         t2.start();
-        System.out.println("program end");
+        System.out.println("program end");*/
         /*HungrySingleton instance = HungrySingleton.getInstance();
         // 如果将这个 instance 序列号到一个文件中，然后再从这个文件中取出来，这两个对象还是同一个对象吗？
         ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("single_file"));
@@ -60,7 +60,7 @@ public class Test {
 
 
         // 枚举 序列化测试 序列化和反序列化不会影响枚举类型的单例
-       /* EnumInstance instance = EnumInstance.getInstance();
+        EnumInstance instance = EnumInstance.getInstance();
         instance.setData(new Object());
 
         ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("single_file"));
@@ -71,7 +71,7 @@ public class Test {
         EnumInstance newInstance = (EnumInstance) ois.readObject();
         System.out.println(instance.getData());
         System.out.println(newInstance.getData());
-        System.out.println(instance == newInstance);*/
+        System.out.println(instance == newInstance);
 
 
         //枚举 测试 反射
